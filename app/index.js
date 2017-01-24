3b0fd9e099cf3a50e54918d4fd973038c9e9f828
Pavel Šuraba <pavel.suraba@symbiodigital.com>
2017-01-24 10:49:12 +0100

    typo

--- app/index.js
+++ app/index.js
@@ -3,6 +3,6 @@ import {render} from 'react-dom';
 
 import globalStyles from '_utils/global-styles';
 import GoogleMap from 'containers/GoogleMap/GoogleMap';
-import KEY from './apiKej.js'
+import KEY from './apiKey.js'
 
 render(<GoogleMap key={KEY}/>, document.querySelector('#app'));
